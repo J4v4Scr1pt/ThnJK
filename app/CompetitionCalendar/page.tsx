64d8fafd2be0b0e3162a8f3fc87c668b56096df9
TomasÖ <Tomas stlind>
2024-09-04 00:14:54 +0200

    updated packages fixed zod for membership and updated calandar to latest

--- app/CompetitionCalendar/page.tsx
+++ app/CompetitionCalendar/page.tsx
@@ -6,20 +6,34 @@ export default function CompetitionCalendarPage() {
 		<div className="ml-auto mr-auto max-w-[1200px] p-3">
 			<section>
 				<h1 className="text-3xl">Tävlingskalender</h1>
-                <br />
+				<br />
 				<p>
-				<b>Förtydligande om avgifter:</b> Från och med höstterminen 2023 gäller att man anmäler sig före deadline i Smoothcomp för Early bird avgift om det finns eller standard avgift om det inte finns Early Bird. 
-                Det innebär att klubben går in efter deadline och godkänner/betalar alla anmälningar. Görs en anmälan efter detta kommer klubben inte godkänna eller betala anmälan om man inte kan visa upp kvitto/bevis på att man själv betalt in hela avgiften till Trollhättans Judoklubb. 
-                <b className='text-red-500 underline'> Anmälan i god tid och avanmälan med giltigt skäl stöttar vi. Sen anmälan och ogiltig avanmälan/ej dyker upp stöttar vi inte, utan medlemmen får stå för alla avgifter.</b>
+					<b>Förtydligande om avgifter:</b> Från och med höstterminen 2023 gäller att man anmäler
+					sig före deadline i Smoothcomp för Early bird avgift om det finns eller standard avgift om
+					det inte finns Early Bird. Det innebär att klubben går in efter deadline och
+					godkänner/betalar alla anmälningar. Görs en anmälan efter detta kommer klubben inte
+					godkänna eller betala anmälan om man inte kan visa upp kvitto/bevis på att man själv
+					betalt in hela avgiften till Trollhättans Judoklubb.
+					<b className="text-red-500 underline">
+						{' '}
+						Anmälan i god tid och avanmälan med giltigt skäl stöttar vi. Sen anmälan och ogiltig
+						avanmälan/ej dyker upp stöttar vi inte, utan medlemmen får stå för alla avgifter.
+					</b>
 				</p>
-				<ul className="my-8 list-disc">
-					<li>Anmälan görs individuellt i Smoothcomp (betalning klickar du förbi) Önskar du hjälp med Smoothcomp be ledarna på klubben om hjälp.</li>
-					<li>Alla anmälningsavgifter på <b>prioriterad</b> tävling/läger betalas av klubben. OM anmälan är gjord före utgången av Early/Standard avgift.</li>
+				<ul className="my-8 list-inside list-disc">
+					<li>
+						Anmälan görs individuellt i Smoothcomp (betalning klickar du förbi) Önskar du hjälp
+						med Smoothcomp be ledarna på klubben om hjälp.
+					</li>
+					<li>
+						Alla anmälningsavgifter på <b>prioriterad</b> tävling/läger betalas av klubben. OM
+						anmälan är gjord före utgången av Early/Standard avgift.
+					</li>
 					<li>Läs vår policy avseende avgifter och ledare vid läger och tävling.</li>
 				</ul>
 
 				<Image
-					className="h-[500px] w-screen shadow-large dark:shadow-none"
+					className="h-[700px] w-screen shadow-large dark:shadow-none"
 					{...{
 						alt: 'INFO-skarmdump-2023',
 						src: Tavlingskalender,
@@ -27,7 +41,7 @@ export default function CompetitionCalendarPage() {
 						priority: true,
 						quality: 100,
 						width: 738,
-						height: 431,
+						height: 700,
 					}}
 				/>
 			</section>
--- app/Membership/MembershipForm.tsx
+++ app/Membership/MembershipForm.tsx
@@ -11,24 +11,61 @@ import {
 	Textarea,
 	useDisclosure,
 } from '@nextui-org/react';
+import { useFormState, useFormStatus } from 'react-dom';
+import { membershipSchema } from './membershipSchema';
 
 export function MembershipForm() {
-	const { isOpen, onOpen, onOpenChange } = useDisclosure();
+	const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
+	const onSubmitAction = async (prevState: formState, data: FormData) => {
+		const formData = Object.fromEntries(data);
+		const parsed = membershipSchema.safeParse(formData);
+		if (!parsed.success) {
+			const errors = parsed.error?.format();
+			return errors;
+		}
+
+		if (parsed.success) {
+			onClose();
+		}
+		return null;
+	};
+
+	const [formState, formAction] = useFormState(onSubmitAction, null);
 	return (
 		<>
 			<Button className="my-4" onPress={onOpen} color="primary">
 				Bli medlem
 			</Button>
 			<Modal isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={false} scrollBehavior="inside">
-				<ModalContent as="form" action="">
+				<ModalContent as="form" action={formAction}>
 					{(onClose) => (
 						<>
 							<ModalHeader className="flex flex-col gap-1">Bli medlem</ModalHeader>
 							<ModalBody>
 								<div className="flex max-w-96 flex-col gap-3">
-									<DatePicker isRequired label="Datum" />
-									<Input isRequired label="Förnamn" type="text" />
-									<Input isRequired label="Efternamn" type="text" />
+									<DatePicker
+										isRequired
+										label="Datum"
+										name="date"
+										isInvalid={Boolean(formState?.date)}
+										errorMessage={formState?.date?._errors[0]}
+									/>
+									<Input
+										isRequired
+										label="Förnamn"
+										type="text"
+										name="firstName"
+										isInvalid={Boolean(formState?.firstName)}
+										errorMessage={formState?.firstName?._errors[0]}
+									/>
+									<Input
+										isRequired
+										label="Efternamn"
+										type="text"
+										name="lastName"
+										isInvalid={Boolean(formState?.lastName)}
+										errorMessage={formState?.lastName?._errors[0]}
+									/>
 									<Input
 										classNames={{
 											input: '[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none',
@@ -36,9 +73,26 @@ export function MembershipForm() {
 										isRequired
 										label="Personnummer"
 										type="number"
+										name="socialSecurityNumber"
+										isInvalid={Boolean(formState?.socialSecurityNumber)}
+										errorMessage={formState?.socialSecurityNumber?._errors[0]}
 									/>
-									<Input isRequired label="Gatuadress" type="text" />
-									<Input isRequired label="Postadress" type="text" />
+									<Input
+										isRequired
+										label="Gatuadress"
+										type="text"
+										name="streetAddress"
+										isInvalid={Boolean(formState?.streetAddress)}
+										errorMessage={formState?.streetAddress?._errors[0]}
+									/>
+									<Input
+										isRequired
+										label="Postadress"
+										type="text"
+										name="mailingAddress"
+										isInvalid={Boolean(formState?.mailingAddress)}
+										errorMessage={formState?.mailingAddress?._errors[0]}
+									/>
 									<Input
 										classNames={{
 											input: '[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none',
@@ -46,18 +100,35 @@ export function MembershipForm() {
 										isRequired
 										label="Telefonnummer"
 										type="number"
+										name="phoneNumber"
+										isInvalid={Boolean(formState?.phoneNumber)}
+										errorMessage={formState?.phoneNumber?._errors[0]}
 									/>
-									<Input isRequired label="E-post" type="email" />
-									<Textarea
+									<Input
 										isRequired
+										label="E-post"
+										type="email"
+										name="eMail"
+										isInvalid={Boolean(formState?.eMail)}
+										errorMessage={formState?.eMail?._errors[0]}
+									/>
+									<Textarea
 										label="Allergi/Övrigt"
 										description="Prata med tränaren om det är något vi behöver känna till"
 										type="text"
+										name="otherInfo"
 									/>
 									<span>
 										Om något händer är det tryggt för oss tränare att nå någon anhörig:
 									</span>
-									<Input isRequired label="Namn" type="text" />
+									<Input
+										isRequired
+										label="Namn"
+										type="text"
+										name="relativeName"
+										isInvalid={Boolean(formState?.relativeName)}
+										errorMessage={formState?.relativeName?._errors[0]}
+									/>
 									<Input
 										classNames={{
 											input: '[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none',
@@ -65,6 +136,9 @@ export function MembershipForm() {
 										isRequired
 										label="Telefonnummer"
 										type="number"
+										name="relativePhoneNumber"
+										isInvalid={Boolean(formState?.relativePhoneNumber)}
+										errorMessage={formState?.relativePhoneNumber?._errors[0]}
 									/>
 									<Input
 										classNames={{
@@ -73,14 +147,21 @@ export function MembershipForm() {
 										isRequired
 										label="Personnummer"
 										type="number"
+										name="relativeSocialSecurityNumber"
+										isInvalid={Boolean(formState?.relativeSocialSecurityNumber)}
+										errorMessage={formState?.relativeSocialSecurityNumber?._errors[0]}
 									/>
-									<Input isRequired label="E-post" type="email" />
+									<Input
+										isRequired
+										label="E-post"
+										type="email"
+										name="relativeEmail"
+										isInvalid={Boolean(formState?.relativeEmail)}
+										errorMessage={formState?.relativeEmail?._errors[0]}
+									/>
 								</div>
 							</ModalBody>
-							<ModalFooter>
-								<Button onPress={onClose}>Avbryt</Button>
-								<Button color="primary">Skicka</Button>
-							</ModalFooter>
+							<FooterContent onClose={onClose} />
 						</>
 					)}
 				</ModalContent>
@@ -88,3 +169,17 @@ export function MembershipForm() {
 		</>
 	);
 }
+
+const FooterContent = ({ onClose }: { onClose: () => void }) => {
+	const { pending } = useFormStatus();
+	return (
+		<ModalFooter>
+			<Button isLoading={pending} onPress={onClose}>
+				Avbryt
+			</Button>
+			<Button isLoading={pending} color="primary" type="submit">
+				Skicka
+			</Button>
+		</ModalFooter>
+	);
+};
