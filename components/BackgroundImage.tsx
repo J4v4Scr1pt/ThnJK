db35da2de222e3eb2bcdc0e11b2ae6536ec284f1
TomasÖ <tomasostlind@gmail.com>
2025-01-05 21:11:58 +0100

    test with bg

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,29 +1,32 @@
 'use client';
 import { useTheme } from 'next-themes';
-import { getImageProps } from 'next/image';
+import Image from 'next/image';
+// import { getImageProps } from 'next/image';
 import { useEffect, useState } from 'react';
+import JudoBgWhite from '@/public/images/JudoBgWhite.webp';
+import JudoBgBlack from '@/public/images/JudoBgBlack.webp';
 
-const common = {
-	alt: 'Background Image',
-	width: 1428,
-	height: 2792,
-	sizes: '100vw',
-	priority: true,
-	quality: 100,
-};
-const {
-	props: { srcSet: dark },
-} = getImageProps({
-	...common,
-	src: '/images/JudoBgWhite.webp',
-});
-const {
-	props: { srcSet: light },
-} = getImageProps({
-	...common,
-	quality: 100,
-	src: '/images/JudoBgBlack.webp',
-});
+// const common = {
+// 	alt: 'Background Image',
+// 	width: 1428,
+// 	height: 2792,
+// 	sizes: '100vw',
+// 	priority: true,
+// 	quality: 100,
+// };
+// const {
+// 	props: { srcSet: dark },
+// } = getImageProps({
+// 	...common,
+// 	src: '/images/JudoBgWhite.webp',
+// });
+// const {
+// 	props: { srcSet: light },
+// } = getImageProps({
+// 	...common,
+// 	quality: 100,
+// 	src: '/images/JudoBgBlack.webp',
+// });
 
 export default function BackgroundImage() {
 	const { theme } = useTheme();
@@ -32,12 +35,16 @@ export default function BackgroundImage() {
 		setMounted(true);
 	}, []);
 	if (!mounted) return;
-
-	return (
-		<picture>
-			<source srcSet={theme === 'light' ? light : dark} />
-			<img
+	if (theme === 'light')
+		return (
+			<Image
 				alt="Background Image"
+				src={JudoBgBlack}
+				placeholder="blur"
+				width={1428}
+				height={2792}
+				sizes="100vw"
+				priority
 				style={{
 					position: 'fixed',
 					height: '100vh',
@@ -47,6 +54,42 @@ export default function BackgroundImage() {
 					opacity: 0.3,
 				}}
 			/>
-		</picture>
-	);
+		);
+	if (theme === 'dark')
+		return (
+			<Image
+				alt="Background Image"
+				src={JudoBgWhite}
+				placeholder="blur"
+				width={1428}
+				height={2792}
+				sizes="100vw"
+				priority
+				style={{
+					position: 'fixed',
+					height: '100vh',
+					width: '100vw',
+					zIndex: '-10',
+					objectFit: 'contain',
+					opacity: 0.3,
+				}}
+			/>
+		);
+
+	// return (
+	// 	<picture>
+	// 		<source srcSet={theme === 'light' ? light : dark} />
+	// 		<img
+	// 			alt="Background Image"
+	// 			style={{
+	// 				position: 'fixed',
+	// 				height: '100vh',
+	// 				width: '100vw',
+	// 				zIndex: '-10',
+	// 				objectFit: 'contain',
+	// 				opacity: 0.3,
+	// 			}}
+	// 		/>
+	// 	</picture>
+	// );
 }
