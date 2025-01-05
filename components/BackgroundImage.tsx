21dcad3fb964dab5db51fc41d05929ad6cd36d02
TomasÖ <tomasostlind@gmail.com>
2025-01-05 21:16:18 +0100

    removed comment out code

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,32 +1,9 @@
 'use client';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
-// import { getImageProps } from 'next/image';
 import { useEffect, useState } from 'react';
 import JudoBgWhite from '@/public/images/JudoBgWhite.webp';
 import JudoBgBlack from '@/public/images/JudoBgBlack.webp';
-
-// const common = {
-// 	alt: 'Background Image',
-// 	width: 1428,
-// 	height: 2792,
-// 	sizes: '100vw',
-// 	priority: true,
-// 	quality: 100,
-// };
-// const {
-// 	props: { srcSet: dark },
-// } = getImageProps({
-// 	...common,
-// 	src: '/images/JudoBgWhite.webp',
-// });
-// const {
-// 	props: { srcSet: light },
-// } = getImageProps({
-// 	...common,
-// 	quality: 100,
-// 	src: '/images/JudoBgBlack.webp',
-// });
 
 export default function BackgroundImage() {
 	const { theme } = useTheme();
@@ -75,21 +52,4 @@ export default function BackgroundImage() {
 				}}
 			/>
 		);
-
-	// return (
-	// 	<picture>
-	// 		<source srcSet={theme === 'light' ? light : dark} />
-	// 		<img
-	// 			alt="Background Image"
-	// 			style={{
-	// 				position: 'fixed',
-	// 				height: '100vh',
-	// 				width: '100vw',
-	// 				zIndex: '-10',
-	// 				objectFit: 'contain',
-	// 				opacity: 0.3,
-	// 			}}
-	// 		/>
-	// 	</picture>
-	// );
 }
