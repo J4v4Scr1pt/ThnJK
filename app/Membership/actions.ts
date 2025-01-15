b83d1e116a722170d3adb3e2232c7645f4779238
TomasÖ <tomasostlind@gmail.com>
2025-01-15 19:55:42 +0100

    forgon to uncomment testing

--- app/Membership/actions.ts
+++ app/Membership/actions.ts
@@ -3,24 +3,21 @@
 import { Membership } from './types';
 
 export async function sendEmail(membership: Membership) {
-	await new Promise((resolve) => setTimeout(resolve, 3000));
-
-	return { success: true };
-	// const response = await fetch('https://api.web3forms.com/submit', {
-	// 	method: 'POST',
-	// 	headers: {
-	// 		'Content-Type': 'application/json',
-	// 		Accept: 'application/json',
-	// 	},
-	// 	body: JSON.stringify({
-	// 		access_key: 'b48528f2-99a1-4634-9a87-451505c8fa82', // TODO: put in env
-	// 		subject: 'Medlemskap',
-	// 		...membership,
-	// 	}),
-	// });
-	// const result = await response.json();
-	// //TODO handle if error
-	// if (result.success) {
-	// 	return { success: true };
-	// }
+	const response = await fetch('https://api.web3forms.com/submit', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+		},
+		body: JSON.stringify({
+			access_key: 'b48528f2-99a1-4634-9a87-451505c8fa82', // TODO: put in env
+			subject: 'Medlemskap',
+			...membership,
+		}),
+	});
+	const result = await response.json();
+	//TODO handle if error
+	if (result.success) {
+		return { success: true };
+	}
 }
